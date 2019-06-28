9696d52d1d1ca23e41f9b9a0330666574b73e36e
Christopher P. Dundon <chris.dundon@gmail.com>
2019-06-28 00:40:41 -0400

    Have POST and GET running.  Great progress today.  Things are coming together...

--- app/controllers/vessels_controller.rb
+++ app/controllers/vessels_controller.rb
@@ -1,8 +1,19 @@
 class VesselsController < ApplicationController
-	def index
-		@vessels = Vessel.all
-		
-		render json: @vessels
+  def index
+    @vessels = Vessel.all
+
+    render json: @vessels
+  end
+
+	def create
+		@vessel = Vessel.create(vessel_params)
+		render json: @vessel
 	end
 
+private
+
+	def vessel_params
+		params.require(:vessel).permit(:name, :operator, :splash_date, :vessel_id)
+	end
+	
 end
