af6bd62c2089a210c06ab966bd2afd9e898301a0
Christopher P. Dundon <chris.dundon@gmail.com>
2019-07-02 01:44:43 -0400

    Added styling.  Getting close to done.

--- app/serializers/vessel_serializer.rb
+++ app/serializers/vessel_serializer.rb
@@ -4,8 +4,12 @@ class VesselSerializer < ActiveModel::Serializer
 	def skinny 
 		activeSkinnies = self.object.skinnies.select { |hash| hash[:active] }
     activeSkinnies.map do |thought|
-      {thought: thought.thought, 
-       skinny_id: thought.skinny_id}
+      {	thought: thought.thought, 
+				skinny_id: thought.skinny_id,
+				active: thought.active,
+				id: thought.id,
+				vessel_id: thought.vessel_id	
+			}
     end 
   end 
 end
